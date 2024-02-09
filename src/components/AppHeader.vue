a3de46230cb03b6690c17d1b5b2dd332b7258648
Alice-Rossi-03 <alicerossi0310@gmail.com>
2024-02-09 16:30:24 +0100

    feat (AppHeader) /add-header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,38 @@
+<script>
+    export default {
+        name: 'AppHeader'
+    }
+</script>
+
+<template>
+    <header>
+        <figure>
+            <img src="../assets/img/logo.png" alt="">
+        </figure>
+        <h1>
+            Yu-Gi-Oh App 
+        </h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+
+    @use '../styles/partials/variables' as *; 
+
+    header{
+        display: flex;
+        padding-block: 0.5em;
+        background-color: $white;
+
+        figure{
+            width: 10%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            img{
+                width: 60%;
+
+            }
+        }
+    }
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'AppMain'
+}
+</script>
+
+<template>
+    <main>
+
+    </main>
+</template>
+
+<style scoped lang="scss">
+    @use '../styles/partials/variables' as *; 
+    
+    main{
+        background-color: $orange;
+    }
+
+</style>
